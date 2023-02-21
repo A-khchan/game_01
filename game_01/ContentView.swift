b08c52684fdd90d3f26d6b12db9c021f352dc52c
Kong Hop Chan <albert88hk@yahoo.com.hk>
2023-02-21 11:38:48 -0800

    Initial Commit

---
+++ game_01/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  game_01
+//
+//  Created by Albert Chan on 2/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ game_01/game_01App.swift
@@ -0,0 +1,17 @@
+//
+//  game_01App.swift
+//  game_01
+//
+//  Created by Albert Chan on 2/21/23.
+//
+
+import SwiftUI
+
+@main
+struct game_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
