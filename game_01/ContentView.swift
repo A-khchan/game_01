a9c357d25366a039b99a48ae99a65610b0f4f741
Kong Hop Chan <albert88hk@yahoo.com.hk>
2023-02-21 12:20:09 -0800

    first branch

--- game_01/ContentView.swift
+++ game_01/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, there!")
+            Text("Hello, first_branch!")
         }
         .padding()
     }
